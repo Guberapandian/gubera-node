e0683fe4a10c627cbe83a6d32a4ae763104cb780
Guberapandian <139293210+Guberapandian@users.noreply.github.com>
2024-10-14 12:38:43 +0530

    Create app.js
    1

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Serve static files from the "views" directory
+app.use(express.static('views'));
+
+// Define a simple route
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Node.js app listening at http://localhost:${port}`);
+});
